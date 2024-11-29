067cf75ccc02a8109337a4a9dfac33688a6ff9f5
Rogier Verkaik <Rogier@getroadmap.com>
2024-11-29 16:04:53 +0100

    Add project files.

---
+++ AoC2024/Day_01.cs
@@ -0,0 +1,16 @@
+﻿using AoCHelper;
+
+namespace AoC2024;
+
+internal class Day_01 : BaseDay
+{
+	public override ValueTask<string> Solve_1()
+	{
+		return new ValueTask<string>("253");
+	}
+
+	public override ValueTask<string> Solve_2()
+	{
+		return new ValueTask<string>("133");
+	}
+}
---
+++ AoC2024/Program.cs
@@ -0,0 +1,3 @@
+﻿using AoCHelper;
+
+await Solver.SolveAll();
