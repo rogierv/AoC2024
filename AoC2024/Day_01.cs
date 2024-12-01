160e61143f8ba88ce443abac3fcbe36489a339a4
Rogier Verkaik <Rogier@getroadmap.com>
2024-12-01 10:28:16 +0100

    Day 01

--- AoC2024/Day_01.cs
+++ AoC2024/Day_01.cs
@@ -6,11 +6,57 @@ internal class Day_01 : BaseDay
 {
 	public override ValueTask<string> Solve_1()
 	{
-		return new ValueTask<string>("253");
+		var lines = File.ReadAllLines(base.InputFilePath);
+
+		List<int> xs = [];
+		List<int> ys = [];
+
+		foreach (var line in lines)
+		{
+			var (x, y) = Parse(line);
+
+			xs.Add(x);
+			ys.Add(y);
+		}
+
+		xs.Sort();
+		ys.Sort();
+
+		var result = xs
+			.Zip(ys)
+			.Aggregate(0, (seed, zip) => seed + Math.Abs(zip.First - zip.Second))
+			.ToString();
+
+		return new(result);
 	}
 
 	public override ValueTask<string> Solve_2()
 	{
-		return new ValueTask<string>("133");
+		var lines = File.ReadAllLines(base.InputFilePath);
+
+		List<int> xs = [];
+		Dictionary<int, int> dict = [];
+
+		foreach (var line in lines)
+		{
+			var (x, y) = Parse(line);
+
+			xs.Add(x);
+
+			var value = dict.GetValueOrDefault(y, 0);
+			dict[y] = value + 1;
+		}
+
+		var result = xs
+			.Aggregate(0, (seed, x) => seed + (x * dict.GetValueOrDefault(x, 0)))
+			.ToString();
+
+		return new(result);
+	}
+
+	private static (int x, int y) Parse(string s)
+	{
+		var split = s.Split("   ");
+		return (int.Parse(split[0]), int.Parse(split[1]));
 	}
 }
